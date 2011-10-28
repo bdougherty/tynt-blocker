7111f13b8b2a2c01634592a61e266b52bc108a2b
Gun.io Whitespace Robot <contact@gun.io>
2011-10-27 21:40:37 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- tyntblock.js
+++ tyntblock.js
@@ -9,12 +9,12 @@ function allowedToLoad(url, nodeName) {
 
 function handleBeforeLoadEvent(event) {
 	const element = event.target;
-	
+
 	// Return now if the content is allowed
 	if (allowedToLoad(event.url, element.nodeName)) {
 		return;
 	}
-	
+
 	// Block the fuckers
 	event.preventDefault();
 }
